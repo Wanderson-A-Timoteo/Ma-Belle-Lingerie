d258d1bcd1b5b0d015b97d63991e9225ff57e792
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-10-05 09:46:19 -0400

    Criado cabecalho.php e add css

---
+++ cabecalho.php
@@ -0,0 +1,33 @@
+<div id="cabecalho_superior">
+  <nav id="menu-cima">
+    <li><a href="#">Minha Conta</a></li>
+    <li><a href="#">Meu Carrinho</a></li>
+    <li><a href="#">Logar</a></li>
+    <li><a href="#">Cadastrar</a></li>
+  </nav>
+</div>
+
+<div id="cabecalho_meio">
+  <h1>Wanderson Timóteo - Desenvolvedor de Sistemas</h1>
+  <p>Nunhum item no seu carrinho de compras</p>
+  <section>
+    <form action="">
+      <label>
+        <span>Buscar</span>
+        <input type="search" name="pesquisa" id="pesquisa">
+        <input type="image" src="imagens/lupa.png" alt="">
+      </label>
+    </form>
+  </section>
+</div>
+
+<div id="cabecalho_inferior">
+  <nav id="menu-principal">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Produtos</a></li>
+      <li><a href="#">Fale Conosco</a></li>
+      <li><a href="#">Quem Somos</a></li>
+    </ul>
+  </nav>
+</div>
--- index.php
+++ index.php
@@ -4,21 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loja Virtual</title>
-    <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" href="css/css.css">
   </head>
   <body>
-    <div id="Principal">
-      <div id="topo">
+    <section id="Principal">
+      <section id="cabecalho">
         <?php include_once("cabecalho.php"); ?>
-      </div> <!-- Fim topo-->
+      </section><!-- Fim topo-->
 
-      <div id="corpo">
-        corpo
-      </div><!-- Fim corpo-->
+      <section id="corpo">
+        
+      </section ><!-- Fim corpo-->
 
-      <div id="rodape">
-        rodape
-      </div><!-- Fim rodape-->
-    </div><!-- Fim Principal-->
+      <footer  id="rodape">
+        
+      </footer><!-- Fim rodape-->
+    </section><!-- Fim Principal-->
   </body>
 </html>
