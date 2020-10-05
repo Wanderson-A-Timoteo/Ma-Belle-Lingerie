c2b7bdc1ec195530d28737bddb81f0f10a9a97a1
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-10-05 09:20:53 -0400

    criado index e apenas a estrutura do layout da home, add todas as imagens que serão usadas

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja Virtual</title>
+    <link rel="stylesheet" href="css.css">
+  </head>
+  <body>
+    <div id="Principal">
+      <div id="topo">
+        <?php include_once("cabecalho.php"); ?>
+      </div> <!-- Fim topo-->
+
+      <div id="corpo">
+        corpo
+      </div><!-- Fim corpo-->
+
+      <div id="rodape">
+        rodape
+      </div><!-- Fim rodape-->
+    </div><!-- Fim Principal-->
+  </body>
+</html>
