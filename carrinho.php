83fc1e6a288d4d67afe6fc54a63c5091a22fd21f
Wanderson Timóteo <wanderson_timoteo@hotmail.com>
2020-12-08 10:53:37 -0400

    add carrinho de compras

--- carrinho.php
+++ carrinho.php
@@ -1,6 +1,38 @@
 <div id="base-carrinho">
-	<h2 class="fundo_azul"> <img src="/imagens/barra-carrinho.png"> </h2>
-	<h3> <img src="/imagens/meu-carrinho.png"> </h3>
+	<h2> <img src="imagens/barra_carrinho01.png"> </h2>
+	<h3> <img src="imagens/meu_carrinho.png"> </h3>
 	<div class="dados-carrinho">
-	<span> para excluir coloque a quantidade zero e clique em alterar </span>
+		<span> para excluir coloque a quantidade zero e clique em alterar </span>
+		<form name="frm_carrinho" action="">
+			<table cellPadding="0" cellSpacing="0" border="1">
+				<thead>
+					<tr>
+						<th> Descrição do produto </th>
+						<th> Quantidade </th>
+						<th> Preço unitário </th>
+						<th> Subtotal </th>
+						<th> Alterar </th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr>
+						<td>
+							<img src="imagems/25.png" alt="">
+							<strong>Conjunto de renda preto</strong>
+						</td>
+						<td> 01 </td>
+						<td> 37,90 </td>
+						<td> <input type="number" id="alterar" name="alterar" value="1" size="3" maxLength="3" min="0" max="100" step="1"> </td>
+						<td> <input type="submit" name="Alterar" value="Alterar"> </td>
+					</tr>
+
+					<tr>
+						<td colSpan="5">Valor total</td>
+					</tr>
+				</tbody>
+
+			</table>
+		</form>
+	</div>
 </div>
